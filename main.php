5554b0848ca7e72d6382504e85809caee76acb48
louden54 <louden_bout@msn.com>
2017-03-15 10:47:59 +0100

    busreis

--- main.php
+++ main.php
@@ -1,2 +1,15 @@
 <?php
-    echo 'Hello world!';
+    $leeftijd = 66;
+
+    if($leeftijd > 65){
+      echo  "reiskosten zijn 5 euro";
+    }else if ($leeftijd < 12){
+        echo "reiskosten zijn 5 euro";
+    }
+    else if ($leeftijd <3){
+       echo "reiskosten zijn gratis";
+    }
+    else {
+       echo "reiskosten zijn 10 euro";
+    }
+    ?>

b778373c585b7da11f7222516cfb158557660d48
louden54 <louden_bout@msn.com>
2017-03-21 13:53:18 +0100

    Zwemclubs

---
+++ Zwemclubs/Zwemclubs.php
@@ -0,0 +1,19 @@
+
+<?php
+    $Zwemclubs = array (
+        array("naam" => "De Spartelkuikens ", "Zwemmers" => 25),
+        array("naam" => "De Waterbuffels ", "Zwemmers" => 32),
+        array("naam" => "Plonsmderin ", "Zwemmers" => 11),
+        array("naam" => "Bommetje ", "Zwemmers" => 23),
+    );
+    foreach($Zwemclubs as $Zwemmers){
+        echo $Zwemmers["naam"];
+        echo $Zwemmers["Zwemmers"];
+        echo "<br>";
+    }
+array("tijd" => "9.30 "   , "kappersagenda" => "Mevr.Willems"),
+    array("tijd" => "9.45 "  , "kappersagenda" => ""),
+    array("tijd"  => "10.00 " , "kappersagenda" => "Paul van den Broek"),
+    array("tijd" => "10.15 " , "kappersagenda" => "Karel de Meeuw"),
+    array("tijd" => "10.30 " , "kappersagenda" => ""),
+
